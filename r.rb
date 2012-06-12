91d377b02ae69d35349c406bf6e077ec05849b22
Ryan Timmons <ryan@ganon.com>
2012-06-12 09:53:44 -0400

    simple tabular/text output

--- r.rb
+++ r.rb
@@ -6,19 +6,62 @@ def prompt_for var
   return _v
 end
 
-
-
-class Scenario
-  attr_accessor :principle, :years, :rate, :annual_addition
-
-  def initialize hsh={}
-    self.principle       = hsh[:principle]
-    self.years           = hsh[:years]
-    self.rate            = hsh[:rate]
-    self.annual_addition = hsh[:annual_addition]
+class Tabular
+  attr_reader :rows
+  def initialize
+    @rows = []
+  end
+  def << row
+    @rows << row
   end
   
-  def compound
+  def maxes 
+    maxes = {}
+    rows.each do |row|
+      row.each_pair do |k,v|
+        maxes[k] = [maxes[k]||k.to_s.length, v.to_s.length].max
+      end
+    end
+    return maxes
+  end
+  
+  def to_s
+    out = []
+    maxes = self.maxes()
+    
+    rows.first.keys.sort.each do |key|
+      out << key.to_s.ljust(maxes[key] + 2)
+    end
+    out << "\n"
+    
+    rows.each do |row|
+      ks = row.keys.sort
+      ks.each do |key|
+        v = row[key]
+        if v.is_a? Float then
+          v = "%.2f" % [v]
+        end
+        out << v.to_s.ljust(maxes[key] + 2)
+      end
+      out << "\n"
+    end
+    
+    out.join('')
+  end
+  
+end
+
+class Scenario
+  attr_reader :principle, :years, :rate, :annual_addition
+
+  def initialize hsh={}
+    @principle       = hsh[:principle]        || 0
+    @years           = hsh[:years]            || 0
+    @rate            = hsh[:rate]             || 1.0
+    @annual_addition = hsh[:annual_addition]  || 0
+  end
+  
+  def end_value
     unless @compounded then
       end_value = self.principle
       self.years.times do |i|
@@ -32,7 +75,28 @@ class Scenario
     return @end_value
   end
   
+  def to_h
+    return {
+      :principle       => self.principle,
+      :years           => self.years,
+      :rate            => self.rate,
+      :annual_addition => self.annual_addition,
+      :end_value       => self.end_value
+    }
+  end
   
+end
+
+if $0 == __FILE__ then
+  t = Tabular.new
+  for r in [1,2,5,7] do
+    t << s = Scenario.new(:principle       => 100_000,
+                          :years           => 40,
+                          :rate            => 1.to_f + (r.to_f/100.to_f),
+                          :annual_addition => 0).to_h
+  end
+  puts t.to_s
+  # puts "%.2f " % [s.end_value]
 end
 
 # principle       = prompt_for("principle").to_i
